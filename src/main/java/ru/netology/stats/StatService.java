af2e06008cc43db9f069efbcf44254430349a00c
Encore <imsolus69@gmail.com>
2024-07-01 01:31:53 +0700

    StatService and StatServiceTest changed

--- src/main/java/ru/netology/stats/StatService.java
+++ src/main/java/ru/netology/stats/StatService.java
@@ -5,8 +5,58 @@ public class StatService {
     public long sumSales(long[] sales) {
         int sum = 0;
         for (long sale : sales) {
-            sum += sale;
+        sum += sale;
         }
         return sum;
     }
+
+    public long avgSales(long[] sales) {
+        long sum = sumSales(sales);
+        return sum / sales.length;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+
+        for(int i = 0; i < sales.length; i++) {
+            if(sales[i] < sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int minMonth = 0;
+
+        for(int i = 0; i < sales.length; i++) {
+            if(sales[i] >= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int countLessThanAvg(long[] sales) {
+        long avg = avgSales(sales);
+        int months = 0;
+        for (long sale : sales) {
+            if(sale < avg) {
+                months++;
+            }
+        }
+        return months;
+    }
+
+    public int countMoreThanAvg(long[] sales) {
+        long avg = avgSales(sales);
+        int months = 0;
+        for (long sale : sales) {
+            if(sale > avg) {
+                months++;
+            }
+        }
+        return months;
+    }
+
 }
--- src/test/java/ru/netology/stats/StatServiceTest.java
+++ src/test/java/ru/netology/stats/StatServiceTest.java
@@ -5,12 +5,86 @@ import org.junit.jupiter.api.Assertions;
 public class StatServiceTest {
 
     @Test
-    public void test() {
+    public void sumSalesTest() {
         StatService statService = new StatService();
         long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
         long actual = statService.sumSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void avgSalesTest() {
+        StatService statService = new StatService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / sales.length;
+        long actual = statService.avgSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSalesTest() {
+        StatService statService = new StatService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int minMonth = 0;
+        long minSales = sales[0];
+        for (int index = 0; index < sales.length; index++) {
+            if(sales[index] < minSales) {
+                minMonth = index + 1;
+                minSales = sales[index];
+            }
+        }
+        long actual = statService.minSales(sales);
+        Assertions.assertEquals(minMonth, actual);
+    }
+
+    @Test
+    public void maxSalesTest() {
+        StatService statService = new StatService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int maxMonth = 0;
+        long maxSales = sales[0];
+        for (int index = 0; index < sales.length; index++) {
+            if(sales[index] >= maxSales) {
+                maxMonth = index + 1;
+                maxSales = sales[index];
+            }
+        }
+        long actual = statService.maxSales(sales);
+        Assertions.assertEquals(maxMonth, actual);
+    }
+
+    @Test
+    public void countLessThanAvgTest() {
+        StatService statService = new StatService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long avg = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / sales.length;
+        long expected = 0;
+        for (long sale : sales) {
+            if(sale < avg) {
+                expected++;
+            }
+        }
+        long actual = statService.countLessThanAvg(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countMoreThanAvgTest() {
+        StatService statService = new StatService();
+        long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long avg = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / sales.length;
+        long expected = 0;
+        for (long sale : sales) {
+            if(sale > avg) {
+                expected++;
+            }
+        }
+        long actual = statService.countLessThanAvg(sales);
         Assertions.assertEquals(expected, actual);
     }
 }
